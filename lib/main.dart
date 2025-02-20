0943fb1609124da66dd5e537eb7ae10ae3fb9154
Rougaax <bangalvian12@gmail.com>
2025-02-20 15:57:27 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,264 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BMICalculatorApp());
+}
+
+class BMICalculatorApp extends StatelessWidget {
+  const BMICalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Calculator',
+      theme: ThemeData(primarySwatch: Colors.cyan),
+      home: const BMIScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class BMIScreen extends StatefulWidget {
+  const BMIScreen({super.key});
+
+  @override
+  State<BMIScreen> createState() => _BMIScreenState();
+}
+
+class _BMIScreenState extends State<BMIScreen> {
+  final TextEditingController heightController = TextEditingController();
+  final TextEditingController weightController = TextEditingController();
+
+  double? bmi;
+  String category = "";
+  String gender = "Laki-laki";
+
+  void calculateBMI() {
+    if (heightController.text.isNotEmpty && weightController.text.isNotEmpty) {
+      double height = double.parse(heightController.text) / 100;
+      double weight = double.parse(weightController.text);
+
+      bmi = weight / pow(height, 2);
+
+      if (bmi! < 18.5) {
+        category = "Kurus";
+      } else if (bmi! >= 18.5 && bmi! < 24.9) {
+        category = "Ideal";
+      } else if (bmi! >= 25 && bmi! < 29.9) {
+        category = "Gemuk";
+      } else {
+        category = "Obesitas";
+      }
+
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: const Text(
+          'BMI Calculator',
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 5,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF2134A8), Color(0xFF4D0AE1)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Masukkan Data',
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            _buildGenderToggle(),
+            const SizedBox(height: 20),
+            _buildInputFields(
+              controller: heightController,
+              label: "Tinggi (cm)",
+              icon: Icons.height,
+            ),
+            const SizedBox(height: 20),
+            _buildInputFields(
+              controller: weightController,
+              label: "Berat (kg)",
+              icon: Icons.monitor_weight,
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: calculateBMI,
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                backgroundColor: Colors.indigo,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                elevation: 8,
+              ),
+              child: const Text(
+                "Hitung BMI",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+            const SizedBox(height: 25),
+            if (bmi != null) _buildResultCard(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildGenderToggle() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildGenderButton("Laki-laki", Icons.male, gender == "Laki-laki"),
+        const SizedBox(width: 20),
+        _buildGenderButton("Perempuan", Icons.female, gender == "Perempuan"),
+      ],
+    );
+  }
+
+  Widget _buildGenderButton(String label, IconData icon, bool isSelected) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          gender = label;
+        });
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.white : Colors.white.withOpacity(0.2),
+          borderRadius: BorderRadius.circular(28),
+          boxShadow: isSelected
+              ? [
+            const BoxShadow(
+              color: Colors.black26,
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ]
+              : [],
+        ),
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              color: isSelected ? Colors.indigoAccent : Colors.white,
+            ),
+            const SizedBox(width: 10),
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: isSelected ? Colors.indigoAccent : Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildResultCard() {
+    return Card(
+      color: Colors.white.withOpacity(0.9),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      elevation: 10,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "BMI Kamu: ${bmi!.toStringAsFixed(2)}",
+              style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Kategori: $category",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: bmi! < 18.5
+                      ? Colors.lightBlue
+                      : (bmi! < 24.9
+                      ? Colors.green
+                      : (bmi! < 29.9
+                      ? Colors.orange
+                      : Colors.red))),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Gender: $gender",
+              style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget _buildInputFields({
+  required TextEditingController controller,
+  required String label,
+  required IconData icon,
+}) {
+  return TextField(
+    controller: controller,
+    keyboardType: TextInputType.number,
+    style: const TextStyle(color: Colors.white),
+    decoration: InputDecoration(
+      labelText: label,
+      labelStyle: const TextStyle(color: Colors.white),
+      prefixIcon: Icon(icon, color: Colors.white),
+      filled: true,
+      fillColor: Colors.white.withOpacity(0.2),
+      enabledBorder: OutlineInputBorder(
+        borderSide: const BorderSide(color: Colors.white),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: const BorderSide(color: Colors.indigo),
+        borderRadius: BorderRadius.circular(15),
+      ),
+    ),
+  );
+}
